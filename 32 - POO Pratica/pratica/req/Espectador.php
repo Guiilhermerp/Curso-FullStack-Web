e9fb7c3125ac815b00c4c84e01f27b10c65aa3fa
Guiilhermerp <guiigrodrigues@gmail.com>
2019-12-04 14:34:45 -0300

    Arquivo prática

---
+++ 32 - POO Pratica/pratica/req/Espectador.php
@@ -0,0 +1,19 @@
+<?php
+
+class Espectador {
+
+	protected $id;
+	protected $email;
+	protected $logado;
+
+	public function __construct(){
+		$this->logado = false;
+		$this->nivel = 3;
+	}
+
+	public function logar($email,$senha){
+		
+	}
+
+	
+}
